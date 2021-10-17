2138f4e0711742ffaf1c51f3c9b24527af81ac32
Niels Kersic <niels@niels.codes>
2021-10-17 13:06:38 +0200

    Add request handler

--- api.py
+++ api.py
@@ -1,22 +1,78 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-import requests
+from PIL import Image
+import os
+import uuid
+import base64
+
+from cgan_mnist import send_file
 
 app = Flask(__name__)
 api = Api(app)
 
 BASE = "http://127.0.0.1:5000/"
 
+def split_string(string): 
+  return list(string)
+
+def merge_images(images):
+  image_count = len(images)
+  image_dimension = 220
+  height = image_dimension
+  width = image_dimension * image_count
+
+  output_img = Image.new(mode="RGB", size=(width, height))
+
+  for idx, path in enumerate(images):
+    img = Image.open(path)
+    img.show()
+    offset_x = idx * image_dimension
+    output_img.paste(img, (offset_x, 0))
+
+  output_path = 'output_image/' + str(uuid.uuid4()) + '.jpg'
+  output_img.save(output_path)
+
+  return output_path
+
+
+def handle_request(value):
+  # TODO: Add error handling
+
+  # split input string into characters
+  chars = split_string(value)
+
+  paths = []
+
+  # convert character to int and use it to generate an image
+  for char in chars:
+    digit = int(char)
+
+    image_path = send_file(digit)
+    paths.append(image_path)
+
+  # merge images
+  merged_img_path = merge_images(paths)
+
+  for path in paths:
+    os.remove(path)
+
+  with open(merged_img_path, 'rb') as merged_img:
+    encoded_img_bytes = base64.b64encode(merged_img.read())
+    encoded_img_string = encoded_img_bytes.decode('utf-8')
+
+  os.remove(merged_img_path)
+
+  return encoded_img_string
+
+
 
 class InputPut(Resource):
     def post(self):
-        print(request.form["Value"])
         userInput = request.form["Value"]
-        return {"Value": userInput}
-
+        encoded_output_img = handle_request(userInput)
+        return {"Value": encoded_output_img}
 
 api.add_resource(InputPut, "/mnistpost")
-
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
