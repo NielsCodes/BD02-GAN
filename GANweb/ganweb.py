f644bbc9d739e1c3aa4f535f293e5533a720556d
Dimitri Batchev <dmtrbtchv@hotmail.com>
2021-10-17 10:09:02 +0200

    base code in api weggehaald

--- GANweb/ganweb.py
+++ GANweb/ganweb.py
@@ -20,9 +20,9 @@ def home_post():
     input = request.form.get("input")
     if(input != ""):
         # SEND data to API
-        #response = requests.post(BASE + "mnistpost", {'Value': input})
-        #response = response.json()
-        response = {'Value': "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="}
+        response = requests.post(BASE + "mnistpost", {'Value': input})
+        response = response.json()
+        #response = {'Value': "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="}
         
         # RECEIVE base64 image from API (and store it in the page)
         #return render_template("index.html", forward_message = response.json())
